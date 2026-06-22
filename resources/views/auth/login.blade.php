6db3af485e9c8ff060c7baf8fb18ad8151b59b7d
Robbi Albert <robbialbert@gmail.com>
2026-06-22 16:34:59 +0700

    feat: implement flexible authentication logic and create themed login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -73,8 +73,8 @@
                     <form action="{{ route('login') }}" method="POST">
                         @csrf
                         <div class="mb-3">
-                            <label for="email" class="form-label">Email</label>
-                            <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" value="{{ old('email') }}" placeholder="nama@email.com">
+                            <label for="email" class="form-label">Email / NIP</label>
+                            <input type="text" name="email" class="form-control @error('email') is-invalid @enderror" id="email" value="{{ old('email') }}" placeholder="nama@email.com atau NIP">
                             @error('email')<div class="invalid-feedback">{{ $message }}</div>@enderror
                         </div>
                         <div class="mb-3">
